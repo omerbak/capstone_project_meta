8653b8e3a5a0ccfeff2986952ad3e20b94964dc4
omerbak <42501059+omerbak@users.noreply.github.com>
2023-02-19 12:24:07 +0300

    add nav fonctionality

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import logo from "../icons_assets/Logo.svg"
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const Nav = () => {
   const [displayMenu, setDisplayMenu] = useState(false);
@@ -14,17 +14,17 @@ const Nav = () => {
     <nav className={displayMenu ?'navbar open-menu' : 'navbar'}>
         <div className="container">
           <div className='mobile-menu'>
-            <Link to="/" className='brand-logo'>
+            <Link to="/" className= 'brand-logo' >
               <img src={logo} alt="little lemon logo" />
             </Link>
             {displayMenu ? <span class="material-symbols-outlined hamberger-menu" onClick={handleMenu}>close</span>
             : <span class="material-symbols-outlined hamberger-menu" onClick={handleMenu}>menu</span>}
           </div>
           <ul className='desk-menu'>
-            <li className='active'>Home</li>
+            <li><NavLink to="/" className={({isActive}) => isActive? 'link active' : 'link' }>Home</NavLink></li>
             <li>About</li>
             <li>Menu</li>
-            <li><Link className='link' to="/booking">Reservations</Link></li>
+            <li><NavLink className={({isActive}) => isActive? 'link active' : 'link' } to="/booking">Reservations</NavLink></li>
             <li>Order Online</li>
             <li>Login</li>
           </ul>
